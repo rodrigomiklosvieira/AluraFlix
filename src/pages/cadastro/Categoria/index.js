f786cd3970165d11b8d4d8bb4de90c62c412ca00
rodrigomiklosvieira <rodrigo.vieira67@etec.sp.gov.br>
2020-08-02 16:10:18 -0300

    correção textarea da descrição

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -70,7 +70,7 @@ function CadastroCategoria() {
 
         <FormField
           label="Descrição:"
-          type="????"
+          type="textarea"
           name="descricao"
           value={values.descricao}
           onChange={handleChange}
