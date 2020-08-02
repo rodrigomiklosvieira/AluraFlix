3d6c0bad3aa214d68bda25078efd6b9d2c193a32
rodrigomiklosvieira <rodrigo.vieira67@etec.sp.gov.br>
2020-08-02 15:59:35 -0300

    atualização url

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -32,7 +32,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if(window.location.href.includes('localhost')) {
-      const URL = 'https://aluraflixrodrigo.herokuapp.com/Categoria'; 
+      const URL = 'http://localhost:8080/categorias'; 
       fetch(URL)
        .then(async (respostaDoServer) =>{
         if(respostaDoServer.ok) {
